46acb18ec239ff9310ddfdfc1479b212c0201748
Blai Bonet <bonetblai@gmail.com>
2019-03-30 11:30:05 +0100

    update make_random_passphrase.bash to pass num blocks in desired passphrase

--- make_random_passphrase.bash
+++ make_random_passphrase.bash
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-python -c "import sys;s=list(sys.argv[1])[:-1];s[3]=s[7]='-';print ''.join(s)" `openssl rand -hex 6`
+if [ "$#" -ne 1 ]; then
+    echo "Usage: make_random_passphrase.bash <num-blocks> (a block consists of 3 hexdigits; blocks are separated by hyphens)"
+    echo "Examples: make_random_passphrase.bash 4"
+    exit
+fi
+
+n=$1
+key=`openssl rand -hex $((2*n))`
+
+n=$n key=$key python - <<EOF
+import sys
+import os
+n = int(os.environ['n'])
+key = list(os.environ['key'])
+for i in range(0,n):
+    key[4*i+3] = '-'
+print (''.join(key))[0:4*n-1]
+EOF
 
